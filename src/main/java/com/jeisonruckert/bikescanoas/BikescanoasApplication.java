b549deaf8936ef3f824c00dc97b4f72dca84eb74
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-24 17:05:50 -0300

    Criando operação de instanciação

--- src/main/java/com/jeisonruckert/bikescanoas/BikescanoasApplication.java
+++ src/main/java/com/jeisonruckert/bikescanoas/BikescanoasApplication.java
@@ -1,13 +1,33 @@
 package com.jeisonruckert.bikescanoas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jeisonruckert.bikescanoas.domain.Categoria;
+import com.jeisonruckert.bikescanoas.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class BikescanoasApplication {
+public class BikescanoasApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BikescanoasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Bicicleta");
+		Categoria cat2 = new Categoria(null, "Patinete");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
