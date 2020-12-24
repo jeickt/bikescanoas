dbdd61a8485cda9e6575568be2bb47b7c837887f
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-24 16:13:19 -0300

    Banco de Dados H2 e criação automática da base de dados

--- src/main/java/com/jeisonruckert/bikescanoas/domain/Categoria.java
+++ src/main/java/com/jeisonruckert/bikescanoas/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.jeisonruckert.bikescanoas.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -12,6 +20,7 @@ public class Categoria implements Serializable {
 	}
 	
 	public Categoria(Integer id, String nome) {
+		super();
 		this.id = id;
 		this.nome = nome;
 	}
