52156cbe7f13bcc763fff3a4bdf831c34c1ae103
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-24 14:46:27 -0300

    Projeto criado

---
+++ src/main/java/com/jeisonruckert/bikescanoas/BikescanoasApplication.java
@@ -0,0 +1,13 @@
+package com.jeisonruckert.bikescanoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikescanoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikescanoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeisonruckert/bikescanoas/BikescanoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeisonruckert.bikescanoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikescanoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
