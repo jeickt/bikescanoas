204a5822d4fcdcf7275f670f68dffedabb3b9a76
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-24 15:21:23 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/jeisonruckert/bikescanoas/resources/CategoriaResource.java
+++ src/main/java/com/jeisonruckert/bikescanoas/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.jeisonruckert.bikescanoas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jeisonruckert.bikescanoas.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Bicicleta");
+		Categoria cat2 = new Categoria(2, "Patinete");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
