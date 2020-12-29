ecb1b7c335f1fca22f13ecaa25a8ed88c272c286
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-28 22:21:38 -0300

    Deleção da Categoria com DELETE

--- src/main/java/com/jeisonruckert/bikescanoas/services/CategoriaService.java
+++ src/main/java/com/jeisonruckert/bikescanoas/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.jeisonruckert.bikescanoas.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jeisonruckert.bikescanoas.domain.Categoria;
 import com.jeisonruckert.bikescanoas.repositories.CategoriaRepository;
+import com.jeisonruckert.bikescanoas.services.exceptions.DataIntegrityException;
 import com.jeisonruckert.bikescanoas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui bicicletas");
+		}
 	}
 	
  }
