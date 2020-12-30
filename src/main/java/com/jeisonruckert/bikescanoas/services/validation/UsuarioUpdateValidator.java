ce0d74ed9b5576f811a5f3b449dcfef72ea11b60
Jeison Leandro Rückert <jeison.let@gmail.com>
2020-12-30 13:07:04 -0300

    Validação customizada: email não repetido na atualização de Usuario

---
+++ src/main/java/com/jeisonruckert/bikescanoas/services/validation/UsuarioUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.jeisonruckert.bikescanoas.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.jeisonruckert.bikescanoas.domain.Usuario;
+import com.jeisonruckert.bikescanoas.dto.UsuarioDTO;
+import com.jeisonruckert.bikescanoas.repositories.UsuarioRepository;
+import com.jeisonruckert.bikescanoas.resources.exceptions.FieldMessage;
+
+public class UsuarioUpdateValidator implements ConstraintValidator<UsuarioUpdate, UsuarioDTO> {
+	
+	@Autowired
+	private UsuarioRepository repo;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(UsuarioUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(UsuarioDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		Usuario aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
